0a45e536960c0810dd02cc2190a06f78813d8885
Charles Korpics <72992136+korpx-z@users.noreply.github.com>
2021-01-29 22:42:41 -0500

    trim

--- odds_evens/main.go
+++ odds_evens/main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
 )
 
 func main() {
-	var soi = []int{
-		1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-	}
-	for _, num := range soi {
-		newNum := num % 2
-		if newNum == 0 {
-			fmt.Printf("%v is an even number", num)
-		} else {
-			fmt.Printf("%v is an odd number", num)
-		}
-	}
+    var soi = []int{
+	1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    }
+    for _, num := range soi {
+	newNum := num % 2
+	    if newNum == 0 {
+		fmt.Printf("%v is an even number", num)
+        	} else {
+		fmt.Printf("%v is an odd number", num)
+	    }
+    }
 }
