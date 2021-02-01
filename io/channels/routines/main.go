706182705ac8f73ebe60a08b075b59fa1d3b0a7b
charles-korpics <charles.korpics@ibm.com>
2021-02-01 14:43:57 -0500

    goroutines

---
+++ io/channels/routines/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	var links []string
+	links = append(links,
+		"http://www.google.com",
+		"https://www.ibm.com",
+	)
+
+	for _, link := range links {
+		checkLink(link)
+	}
+}
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Printf("%+v", err)
+		return
+	}
+	fmt.Printf("%v"+" "+"is up!", link)
+}
